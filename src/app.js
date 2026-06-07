6846bfbe399aa2efbf41326c38e3693638aad2db
dev-deep12 <dev.deepanshu@outlook.com>
2026-06-07 23:25:06 +0530

    fixed cors

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:3000",
       "http://127.0.0.1:5173",
       "http://127.0.0.1:5174",
+      "https://stepper-form-frontend-tau.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
